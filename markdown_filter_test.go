e4e13d7614168b0e3763b601b08c13952fa43a31
Jason Lee <huacnlee@gmail.com>
2020-02-26 23:38:31 +0800

    Add Markdown option example

--- markdown_filter_test.go
+++ markdown_filter_test.go
@@ -7,10 +7,27 @@ import (
 )
 
 func ExampleMarkdownFilter() {
+	// Custom blackfriday HTML render options
+	renderer := blackfriday.NewHTMLRenderer(blackfriday.HTMLRendererParameters{
+		Flags: blackfriday.UseXHTML |
+			blackfriday.NofollowLinks |
+			blackfriday.CompletePage,
+	})
+
+	// Custom blackfriday extensions
+	extensions := blackfriday.Tables |
+		blackfriday.FencedCode |
+		blackfriday.Autolink |
+		blackfriday.Strikethrough |
+		blackfriday.SpaceHeadings |
+		blackfriday.HardLineBreak |
+		blackfriday.NoEmptyLineBeforeBlock
+
 	pipe := NewPipeline([]Filter{
 		MarkdownFilter{
 			Opts: []blackfriday.Option{
-				blackfriday.WithExtensions(blackfriday.AutoHeadingIDs),
+				blackfriday.WithRenderer(renderer),
+				blackfriday.WithExtensions(extensions),
 			},
 		},
 		SanitizationFilter{},
@@ -20,15 +37,34 @@ func ExampleMarkdownFilter() {
 <script>alert;</script>
 <style>body {}</style>
 
+| Name | Location |
+| ---- | --- |
+| Jason | Chengdu |
+
 This is [html-pipeline](https://github.com/huacnlee/html-pipeline) Markdown filter.`
 
 	out, _ := pipe.Call(raw)
 	fmt.Println(out)
 	// Output:
-	// <h1 id="hello-world">Hello world</h1>
+	// <h1>Hello world</h1>
 	//
-	// <p>alert;
+	// <p>alert;<br/>
 	// body {}</p>
 	//
+	// <table>
+	// <thead>
+	// <tr>
+	// <th>Name</th>
+	// <th>Location</th>
+	// </tr>
+	// </thead>
+	//
+	// <tbody>
+	// <tr>
+	// <td>Jason</td>
+	// <td>Chengdu</td>
+	// </tr>
+	// </tbody>
+	// </table>
 	// <p>This is <a href="https://github.com/huacnlee/html-pipeline" rel="nofollow">html-pipeline</a> Markdown filter.</p>
 }
